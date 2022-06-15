152cb967bf85b1cd21c6ad06bc20b68857d8824a
iG Gu <iggu@nm.ru>
2022-06-15 22:26:16 +0300

    apti4: packages lists to install

--- bin/apti4xgui.sh
+++ bin/apti4xgui.sh
@@ -10,19 +10,24 @@ function prepare()
         msg -- "Install GUI applications from PPAs with apt (must be run as root)."
         msg -- ""
         msg -- 'Parameters (no whitespaces in values, + mandatory, - optional):'
-        param  PKG      -p --pkg     --    "- config file with list of packages to install"
+        param  PKG      -p --pkg     --    "- config file with list of packages available to install"
+        param  PCATS    -c --pcats   --    "- comma-separated list of categories of packages to install"
+        flag   PCALL    -C --pcatall --    "- install all categories of packages from list of available"
         param  PPA      -P --ppa     --    "- config file with list of PPAs to import"
         flag   KEYS     -K --keys    --    "- acquire missing keys by their hashes"
         flag   NO_COLOR --no-color   --    "- do not colorize output"
         disp   :usage   -h --help    --    "- print help, ignore any other cliargs"
         msg -- 'PPAs config file is of bash syntax and contains several associative arrays'
         msg -- '    which describes which ppas to import and how to do it'
+        msg -- 'PKGs config file is of bash syntax and contains single assoc array'
+        msg -- '    which contains packages by categories recommended for installation'
     }
     eval "$($THIS_LIB_PATH/getoptions.sh parser_definition) exit 1"
 
     [ -z "$REST" ] || :fail 2 "Extra args detected (param with space?)"
 
-    :capar-rp PKG pkg opt ; :capar-rp PPA ppa opt ; :capar-rp KEYS keys opt ;
+    :capar-rp PKG pkg opt ; :capar PCATS pcats opt; :capar PCALL pcall opt;
+    :capar-rp PPA ppa opt ; :capar-rp KEYS keys opt ;
     for pk in pkg ppa ; do
         local pv="${CliArgs[$pk]}"
         if [[ -n $pv && ! -f $pv ]] ; then
@@ -35,23 +40,70 @@ function prepare()
     :must-be-root
 }
 
+function ppa_import_all()
+{
+    if [ -z "${CliArgs[ppa]}" ] ; then
+        echo -e "${Purple}No PPAs to import${ResetColor}"
+
+    else
+        echo -e "${Cyan}Import PPAs...${ResetColor}"
+        codename=$THIS_UBUNTU_CODENAME
+        source ${CliArgs[ppa]}
+        source $THIS_LIB_PATH/ppa.sh
+        mkdir -p /etc/apt/sources.list.d/
+
+        ppa_import PPA_LAUNCHPAD PPA_LAUNCHPAD lp
+        ppa_import PPA_KEYS_AUTO PPA_REPO_DEBS auto
+        ppa_import PPA_KEYS_SIGN PPA_REPO_DEBS sign
+        ppa_import PPA_KEYS_LP PPA_REPO_DEBS lpker
+    fi
+
+    if [ -n "${CliArgs[keys]}" ] ; then
+        ppa_add_missing_keys
+    fi
+}
+
+function pkg_install_all()
+{
+    if [ -z "${CliArgs[pkg]}" ] ; then
+        echo -e "${Purple}No PKGs to install/recommend (is list of packages provided?)${ResetColor}"
+        return
+    fi
+
+    source ${CliArgs[pkg]}
+    local ag="apt-get install --no-install-recommends"
+
+    if [ -n "${CliArgs[pcall]}" ] ; then
+        echo -e "${Cyan}Installing all recommended packages: ${Yellow}${PKGs[@]}${ResetColor}"
+        $ag ${PKGs[@]}
+
+    elif [ -n "${CliArgs[pcats]}" ] ; then
+        declare install= missing=
+        local cats=`echo ${CliArgs[pcats]} | tr ',' ' '`
+        for cat in $cats ; do
+            if [ -z "${PKGs[$cat]}" ] ; then
+                missing+="$cat "
+            else
+                install+="${PKGs[$cat]} "
+            fi
+        done
+
+        if [ -n "$missing" ] ; then
+            :fail 10 "Unknown package categories:" "$missing"
+        else
+            echo -e "${Cyan}Installing following categories of recommended packages: ${IntenseYellow}${cats}${ResetColor}"
+            echo -e "    > ${Yellow}$install${ResetColor}"
+            $ag $install
+        fi
+
+    else
+        echo -e "${Purple}List of packages recommended to be installed by categories.${ResetColor}"
+        echo "Install what you like by selecting any combination of them with 'sudo apt-get install'"
+        echo "or via this script's command line options"
+        :echo_assarr "PKGs" "IntenseBlack"
+    fi
+}
 
 prepare $*
-
-codename=$THIS_UBUNTU_CODENAME
-if [ -z "${CliArgs[ppa]}" ] ; then
-    echo -e "${Purple}No PPAs to import${ResetColor}"
-else
-    echo -e "${Cyan}Import PPAs...${ResetColor}"
-    source ${CliArgs[ppa]}
-    source $THIS_LIB_PATH/ppa.sh
-    mkdir -p /etc/apt/sources.list.d/
-    ppa_import PPA_LAUNCHPAD PPA_LAUNCHPAD lp
-    ppa_import PPA_KEYS_AUTO PPA_REPO_DEBS auto
-    ppa_import PPA_KEYS_SIGN PPA_REPO_DEBS sign
-    ppa_import PPA_KEYS_LP PPA_REPO_DEBS lpker
-fi
-
-if [ -n "${CliArgs[keys]}" ] ; then
-    ppa_add_missing_keys
-fi
+ppa_import_all
+pkg_install_all
---
+++ conf/pkg-gui.sh
@@ -0,0 +1,14 @@
+
+declare -A PKGs=(
+    [remote]="remmina remmina-plugin-rdp x11vnc"
+    [desktop]="wmctrl xdotool"
+    [code]="gitk kdiff3 codium sublime-text"
+    [files]="dolphin-plugins doublecmd-qt doublecmd-plugins"
+    [media]="w3m-img sxiv imagemagick-6.q16hdri mplayer smplayer smtube qmmp"
+    [dev]="libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev"
+    [term]="xterm yakuake tilix"
+    [noise]="anoise-gui anoise-community-extension1 anoise-community-extension2 anoise-community-extension3 anoise-community-extension4 anoise-community-extension5"
+    [browse]="opera-stable vivaldi-stable brave-browser librewolf"
+    [youtube]="youtube-to-mp3 youtube-downloader lyrics-finder "
+    [office]="goldendict"
+)
