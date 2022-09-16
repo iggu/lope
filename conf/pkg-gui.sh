e0db79277257fbb23f07dc5b27e451b76bba0416
iG Gu <iggu@nm.ru>
2022-09-16 20:59:47 +0300

    configs: minor changes

--- conf/pkg-gui.sh
+++ conf/pkg-gui.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 declare -A PKGs=(
     [remote]="remmina remmina-plugin-rdp x11vnc"
@@ -6,7 +7,7 @@ declare -A PKGs=(
     [files]="dolphin-plugins doublecmd-qt doublecmd-plugins"
     [media]="w3m-img sxiv imagemagick-6.q16hdri mplayer smplayer smtube qmmp ffmpegthumbnailer ffmpeg"
     [dev]="libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev"
-    [term]="xterm yakuake tilix"
+    [term]="xterm yakuake tilix fonts-firacode"
     [noise]="anoise-gui anoise-community-extension1 anoise-community-extension2 anoise-community-extension3 anoise-community-extension4 anoise-community-extension5"
     [browse]="falkon opera-stable vivaldi-stable brave-browser librewolf"
     [youtube]="youtube-to-mp3 youtube-downloader lyrics-finder "
--- conf/pkg-term.sh
+++ conf/pkg-term.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 declare -A PKGs=(
     [sys]="lm-sensors uuid htop"
