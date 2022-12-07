89472e3020c73ce36439f204addb1eddde8f5bf2
iG Gu <iggu@nm.ru>
2022-12-07 23:55:57 +0300

    pkgs tweaks

--- conf/pkg-gui.sh
+++ conf/pkg-gui.sh
@@ -4,12 +4,12 @@ declare -A PKGs=(
     [remote]="remmina remmina-plugin-rdp remmina-plugin-vnc x11vnc"
     [desktop]="wmctrl xdotool"
     [code]="gitk kdiff3 codium sublime-text"
-    [files]="dolphin-plugins doublecmd-qt doublecmd-plugins"
+    [files]="doublecmd-qt doublecmd-plugins"
     [media]="w3m-img sxiv imagemagick-6.q16hdri mplayer smplayer smplayer-themes smtube qmmp ffmpegthumbnailer ffmpeg mpv"
     [dev]="libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev"
-    [term]="xterm yakuake tilix fonts-firacode"
+    [term]="xterm fonts-firacode"
     [noise]="anoise-gui anoise-community-extension2 anoise-community-extension3" # all=basic, 1=boring, 2=places, 3=noises, 4=lakes&rivers, 5=birds
-    [browse]="falkon opera-stable firefox-esr vivaldi-stable brave-browser librewolf chromium-codecs-ffmpeg-extra telegram"
+    [browse]="firefox-esr vivaldi-stable brave-browser librewolf chromium-codecs-ffmpeg-extra telegram"
     [youtube]="youtube-to-mp3 youtube-downloader lyrics-finder "
     [office]="goldendict vym"
 )
--- conf/pkg-term.sh
+++ conf/pkg-term.sh
@@ -6,7 +6,7 @@ declare -A PKGs=(
     [comp]="automake autoconf pkg-config cmake build-essential libtool python3 python3-pip"
     [media]="chafa exif mpg321 vorbis-tools"
     [shell]="tmux silversearcher-ag fd-find ncdu tree mc bash-completion stow jq"
-    [get]="youtube-dl wget curl"
+    [get]="youtube-dl yt-dlp wget curl"
     #[lua]="lua5.1 luajit luarocks liblua5.1-0 liblua5.1-0-dev libapr1 libapr1-dev libaprutil1 libaprutil1-dev libaprutil1-dbd-sqlite3 libapreq2-3 libapreq2-dev"
     [text]="pandoc poppler-utils"
 )
